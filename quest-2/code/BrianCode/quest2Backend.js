7e52cedba58f5e8c8f91d0b1b844795593fb11d0
Brian Homoon Jung <brianhmj@bu.edu>
2022-02-23 20:34:30 -0500

    update

---
+++ quest-2/code/BrianCode/quest2Backend.js
@@ -0,0 +1,42 @@
+var app = require('express')();
+var http = require("http").Server(app);
+const io = require("socket.io");
+
+const { SerialPort, ReadlineParser  } = require("serialport");
+
+var dataStream = []
+
+app.get('/', (req,res) => {
+    res.sendFile(__dirname + '/canvas.html')
+})
+
+function hello(arg) {
+    console.log(arg)
+    pureData = arg.split("\r")[0]
+    splitData = pureData.split(",")
+    dataStream = [Number(splitData[0]),Number(splitData[1]),111]
+    // dataStream.push(pureData.split(","))
+    // console.log(dataStream)
+
+    app.get('/data', (req,res) => {
+        app.emit('dataEvent');
+        res.send(dataStream)
+    })
+
+    console.log("asdf")
+    console.log(dataStream)
+}
+
+
+const port = new SerialPort({
+    path:'COM3', 
+    baudRate: 115200,
+});
+
+const parser = new ReadlineParser()
+port.pipe(parser)
+parser.on('data', hello)
+
+app.listen(8080, () => {
+    console.log("go to http://localhost:8080/");
+});
